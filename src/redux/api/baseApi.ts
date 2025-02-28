e8c7d6200d0ea2f4642b6870a6b7f5202fe5f01c
ihrimon <ihrimon147@gmail.com>
2025-03-01 04:57:38 +0600

    host url added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -11,7 +11,7 @@ import { logout, setUser } from '../features/auth/authSlice';
 import { base_url } from '@/constants';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${base_url}`,
+  baseUrl: 'cycle-planet-store-server.vercel.app/api',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
@@ -39,7 +39,7 @@ const baseQueryWithRefreshToken: BaseQueryFn<
   // }
   if (result?.error?.status === 401) {
 
-    const res = await fetch('http://localhost:8000/api/auth/refresh-token', {
+    const res = await fetch('cycle-planet-store-server.vercel.app/api/auth/refresh-token', {
       method: 'POST',
       credentials: 'include',
     });
